92849d2c768d53ca05f185feffaa3d5f7b60ee1e
Alan Chen <cmjethz@gmail.com>
2024-02-27 00:21:51 +0800

    Create BinarySearchTree.java

---
+++ BinarySearchTree.java
@@ -0,0 +1,73 @@
+class TreeNode {
+    int key;
+    TreeNode left, right;
+
+    public TreeNode(int item) {
+        key = item;
+        left = right = null;
+    }
+}
+
+public class BinarySearchTree {
+    private TreeNode root;
+
+    public BinarySearchTree() {
+        root = null;
+    }
+
+    // 插入节点
+    public void insert(int key) {
+        root = insertRec(root, key);
+    }
+
+    private TreeNode insertRec(TreeNode root, int key) {
+        if (root == null) {
+            root = new TreeNode(key);
+            return root;
+        }
+
+        if (key < root.key) {
+            root.left = insertRec(root.left, key);
+        } else if (key > root.key) {
+            root.right = insertRec(root.right, key);
+        }
+
+        return root;
+    }
+
+    // 搜索节点
+    public boolean search(int key) {
+        return searchRec(root, key);
+    }
+
+    private boolean searchRec(TreeNode root, int key) {
+        if (root == null || root.key == key) {
+            return root != null;
+        }
+
+        if (key < root.key) {
+            return searchRec(root.left, key);
+        } else {
+            return searchRec(root.right, key);
+        }
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree tree = new BinarySearchTree();
+
+        // 插入节点
+        tree.insert(50);
+        tree.insert(30);
+        tree.insert(20);
+        tree.insert(40);
+        tree.insert(70);
+        tree.insert(60);
+        tree.insert(80);
+
+        // 搜索节点
+        System.out.println("Search 20: " + tree.search(20));
+        System.out.println("Search 90: " + tree.search(90));
+    }
+}
+
+
